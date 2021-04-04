5985639394465c17976aa6d4acd09c88f8d47261
gbielanova <gbielanova@gmail.com>
2021-04-04 10:47:55 +0200

    Add task2

--- js/hw2.js
+++ js/hw2.js
@@ -18,3 +18,20 @@ function getSum() {
     document.getElementById("task1_result").innerHTML = result;
 }
 
+function getDivider() {
+    let num1 = Number(document.getElementById('task2_num1').value);
+    let num2 = Number(document.getElementById('task2_num2').value);
+
+    let result = 0;
+
+    (num2 > num1) ? result = num1 : result = num2;
+
+    // we can add check that result > 0 but both numbers should be divided to 1
+    // but it will not work if numbers will be float, hope for the best this time
+    while (!(num2 % result == 0 && num1 % result == 0)) {
+        result -= 1;
+    }
+
+    document.getElementById("task2_result").innerHTML = result;
+}
+
