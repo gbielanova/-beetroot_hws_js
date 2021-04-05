5e79ecf768baa4840b1a3f13ed953f16aca799f7
gbielanova <gbielanova@gmail.com>
2021-04-05 09:53:14 +0200

    Add task2

--- js/hw3.js
+++ js/hw3.js
@@ -1,3 +1,11 @@
 function task1(num1, num2) {
     return num1 < num2 ? -1 : num1 > num2 ? 1 : 0;
 }
+
+function task2(num) {
+    if (num === 0)
+        return 1;
+    else {
+        return num * task2(num - 1);
+    }
+}
