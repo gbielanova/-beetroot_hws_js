8e6bf75d3cbebd5a14b8b0475c1a6fb3ed13895d
gbielanova <gbielanova@gmail.com>
2021-04-07 21:50:54 +0200

    Add part task2

--- js/hw4.js
+++ js/hw4.js
@@ -38,3 +38,57 @@ function getEstimate() {
     let dest = Number(document.getElementById("dest").value);
     document.getElementById("time").innerHTML = `Ви подолаєте цю відстань за ${Car.calcTime(dest)} годин`;
 }
+
+function makeFraction(numerator, denominator) {
+    let fraction = {
+        numerator: numerator,
+        denominator: denominator,
+
+        printFraction() {
+            return `${this.numerator}/${this.denominator}`
+        },
+
+        clear() {
+            let min;
+            this.numerator < this.denominator ? min = this.numerator : min = this.denominator;
+
+            for (let i = 2; i < min; i++) {
+                if (this.numerator % i == 0 && this.denominator % i == 0) {
+                    this.numerator = this.numerator / i;
+                    this.denominator = this.denominator / i;
+                }
+            }
+
+        },
+
+        summ(fraction2) {
+            new_den = this.denominator * fraction2.denominator;
+
+            new_fr1_num = this.numerator * fraction2.denominator;
+
+            new_fr2_num = this.denominator * fraction2.numerator;
+
+            return makeFraction(new_fr1_num + new_fr2_num, new_den);
+        },
+
+
+    }
+
+    return fraction;
+}
+
+function testFunctions() {
+    let fr1 = makeFraction(Number(document.getElementById("fr1_num").value),
+        Number(document.getElementById("fr1_den").value));
+    let fr2 = makeFraction(Number(document.getElementById("fr2_num").value),
+        Number(document.getElementById("fr2_den").value));
+
+    summ_fr = fr1.summ(fr2);
+
+    let summ_fr_cleared = Object.assign({}, summ_fr);
+    summ_fr_cleared.clear();
+
+    document.getElementById("fr_sum").innerHTML = `${fr1.printFraction()} + 
+                                        ${fr2.printFraction()}=${summ_fr.printFraction()}=
+                                        ${summ_fr_cleared.printFraction()}`;
+}
