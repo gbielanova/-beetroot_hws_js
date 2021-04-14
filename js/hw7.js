d49ef07b6e21fcf596d160034585b4a494603596
gbielanova <gbielanova@gmail.com>
2021-04-14 09:17:01 +0200

    Add task2

--- js/hw7.js
+++ js/hw7.js
@@ -46,3 +46,23 @@ function printPlayList() {
         document.getElementById("playlist").appendChild(y);
     });
 }
+
+function showModalWindow() {
+    var modal = document.getElementById("modal");
+    var btn = document.getElementById("openModal");
+    var closeBtn = document.getElementById("closeModal");
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    closeBtn.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
