16f4a8b1adfbe7192de9370f729bfe474b2dd180
gbielanova <gbielanova@gmail.com>
2021-04-27 14:41:26 +0200

    Add show response result

--- js/hw11.js
+++ js/hw11.js
@@ -1,14 +1,25 @@
 const API = 'http://www.omdbapi.com/?apikey=721371';
 
 function printResponse(response) {
-    console.log(response['Error']);
-    console.log(response['Response']);
-
-    if (response['Response'] == 'False') {
+    if (response['Response'] === 'False') {
         $(".result").append(`<p class='error'>${response['Error']}</p>`);
         return;
     }
 
+    $(".result").append(`<ol class='result-list'></ol>`);
+    response['Search'].forEach(element => {
+        console.log(element);
+        let poster_url = element['Poster'];
+        $(".result-list").append(`<li>      
+                                        <aticle class="result-el">
+                                            <div>
+                                                <header class="result-el__header">${element['Title']}</header>
+                                                <p class="result-el__year">${element['Year']}</p>
+                                            </div>
+                                            <img src="${poster_url}" alt="${element['Title']} poster" class="result-el__poster">
+                                        </aticle>
+                                    </li>`);
+    });
 }
 
 window.addEventListener("load", function () {
@@ -21,10 +32,20 @@ window.addEventListener("load", function () {
 
         var req = new XMLHttpRequest();
         req.responseType = 'json';
-        req.open('GET', `${API}&t=${FD.get('name')}&type=${FD.get('type')}`, true);
+        req.open('GET', `${API}&s=${FD.get('name')}&type=${FD.get('type')}`, true);
         req.onload = function () {
             (this.status == 200) ? printResponse(this.response) : alert('Oops! Something went wrong.');
         };
         req.send(null);
     });
+
+
+    var req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open('GET', `${API}&s=avatar&type=movie`, true);
+    req.onload = function () {
+        printResponse(this.response)
+    };
+    req.send(null);
 });
+
