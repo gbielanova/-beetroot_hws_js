af2f43548a765601c2202241875e427eecd48712
gbielanova <gbielanova@gmail.com>
2021-04-27 15:11:20 +0200

    Add show pages

--- js/hw11.js
+++ js/hw11.js
@@ -1,25 +1,38 @@
 const API = 'http://www.omdbapi.com/?apikey=721371';
 
 function printResponse(response) {
+    printPages(response['totalResults']);
+
     if (response['Response'] === 'False') {
         $(".result").append(`<p class='error'>${response['Error']}</p>`);
         return;
     }
 
     $(".result").append(`<ol class='result-list'></ol>`);
+
     response['Search'].forEach(element => {
-        console.log(element);
-        let poster_url = element['Poster'];
+        let poster = (element['Poster'] != 'N/A') ? `<img src="${element['Poster']}" alt="${element['Title']} poster" class="result-el__poster">` : '';
+
         $(".result-list").append(`<li>      
                                         <aticle class="result-el">
                                             <div>
                                                 <header class="result-el__header">${element['Title']}</header>
                                                 <p class="result-el__year">${element['Year']}</p>
                                             </div>
-                                            <img src="${poster_url}" alt="${element['Title']} poster" class="result-el__poster">
+                                            ${poster}
                                         </aticle>
                                     </li>`);
     });
+}
+
+function printPages(totalResults) {
+    $(".result").append(`<ul class='result-pages'></ul `);
+
+    for (let i = 1; i <= Math.ceil(totalResults / 10); i++) {
+        $(".result-pages").append(`<li class="result-pages__page">${i}</li>`);
+    }
+
+
 }
 
 window.addEventListener("load", function () {
